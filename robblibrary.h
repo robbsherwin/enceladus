6db78d555e1334974764d5ec06c2e35159e10fd4
Robb Sherwin <rsherwin@promontech.com>
2019-08-13 17:35:13 -0600

    Using roodylib

--- robblibrary.h
+++ robblibrary.h
@@ -30,7 +30,7 @@ local n
 
         randomArgs[1] = a1, a2, a3, a4, a5, a6, a7
 
-        for (n=7 ; n>0 ; n--) 
+        for (n=7 ; n>0 ; n--)
 	{
                 if (randomArgs[n]): break
         }
@@ -56,12 +56,12 @@ replace EndGame(end_type)
 {
    PrintStatusLine                 ! update one last time
    PrintEndGame(end_type)          ! print appropriate ending message
- 
+
 #ifclear NO_VERBS
    local r
- 
+
    Message(&EndGame, 1,end_type)    ! ask to RESTART, RESTORE, (UNDO), or QUIT
- 
+
    r = -1
    while r = -1
    {
@@ -70,72 +70,11 @@ replace EndGame(end_type)
    if r = -1
          Message(&EndGame, 2,end_type)    ! ask again (more succinctly)
    }
- 
+
    return r
- 
+
 #else   ! i.e., #elseif set NO_VERBS
- 
+
    return 0;
 #endif
-}
- 
-routine ProcessKey(entry,end_type)
-{
-   local r
-   r = -1
-   select entry
-            case "restart", "r"
-            {
-               if restart
-               {
-                  r = 1
-                  PrintStatusline
-               }
-   #ifclear NO_XVERBS
-               else
-                  Message(&DoRestart, 2)   ! failed
-   #endif
- 
-            }
-            case "restore", "e"
-            {
-   #ifclear NO_XVERBS
-               if Perform(&DoRestore)
-                  r = 1
-   #else
-               if restore
-               {
-                  r = 1
-                  PrintStatusline
-                  DescribePlace(location)
-               }
-   #endif
-            }
-   #ifclear NO_UNDO
-            case "undo", "u"
-            {
-               if not UNDO_OFF
-               {
-                  if undo
-                  {
-                     r = true
-                     PrintStatusLine
-                     DescribePlace(location)
-                  }
-   #ifclear NO_XVERBS
-                  else
-                     Message(&DoUndo, 1)  ! failed
-               }
-               else
-                  Message(&DoUndo, 1)
-   #else
-               }
-   #endif
-            }
-   #endif
-            case "quit", "q"
-            {
-               r = 0
-            }
-   return r
 }
